d57f677b799b57f69a54fa68fb0cea248992d5d4
dionpramana <balebali.tech@gmail.com>
2025-02-12 21:46:55 +0700

    feat: test-pdf

---
+++ src/app/test/[id]/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { useParams } from "next/navigation";
+
+export default function Page() {
+  const { id } = useParams();
+  console.log(id);
+  return (
+    <div className="my-[84px]">
+      {" "}
+      <h1>Welcome to download content</h1>
+      <Link target="_blank" href={`/test/${id}/pdf`}>
+        <Button>Download Content</Button>
+      </Link>
+    </div>
+  );
+}
